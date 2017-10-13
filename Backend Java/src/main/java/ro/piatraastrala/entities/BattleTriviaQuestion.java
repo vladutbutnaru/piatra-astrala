d1761ce7b28f31ee143ac512a129b9e2b21d62d0
Vlad.BUTNARU <vlad.butnaru@feel-it-services.com>
2017-10-13 15:44:28 +0300

    * Added Battle Trivia functionality

--- Backend Java/src/main/java/ro/piatraastrala/entities/BattleTriviaQuestion.java
+++ Backend Java/src/main/java/ro/piatraastrala/entities/BattleTriviaQuestion.java
@@ -12,6 +12,24 @@ public class BattleTriviaQuestion {
     private String questionText;
     private int answer;
     private boolean active;
+    private int answerPlayer1;
+    private int answerPlayer2;
+
+    public int getAnswerPlayer1() {
+        return answerPlayer1;
+    }
+
+    public void setAnswerPlayer1(int answerPlayer1) {
+        this.answerPlayer1 = answerPlayer1;
+    }
+
+    public int getAnswerPlayer2() {
+        return answerPlayer2;
+    }
+
+    public void setAnswerPlayer2(int answerPlayer2) {
+        this.answerPlayer2 = answerPlayer2;
+    }
 
     public boolean isActive() {
         return active;
---
+++ Backend Java/src/main/java/ro/piatraastrala/entities/BattleTriviaResult.java
@@ -0,0 +1,31 @@
+package ro.piatraastrala.entities;
+
+public class BattleTriviaResult {
+    private int score1;
+    private int score2;
+    private double damageDone;
+
+    public int getScore1() {
+        return score1;
+    }
+
+    public void setScore1(int score1) {
+        this.score1 = score1;
+    }
+
+    public int getScore2() {
+        return score2;
+    }
+
+    public void setScore2(int score2) {
+        this.score2 = score2;
+    }
+
+    public double getDamageDone() {
+        return damageDone;
+    }
+
+    public void setDamageDone(double damageDone) {
+        this.damageDone = damageDone;
+    }
+}
